03d3342b055ac6c0c4f243cec010d9d5af7f777e
João Vitor <romanijoaovitor@gmail.com>
2025-04-30 22:42:11 -0300

    faz conexão com o MongoDB através do mongoose

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-// Importa o Express
+// Configuracao Inicial
+require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 
 // Midleware para ler JSON
@@ -11,4 +13,17 @@ app.use(
 app.use(express.json());
 
 // Roda o server
-app.listen(3333);
+const DB_USER = process.env.DB_USER;
+const DB_PASSWORD = encodeURIComponent(process.env.DB_PASSWORD);
+
+mongoose
+    .connect(
+        `mongodb+srv://${DB_USER}:${DB_PASSWORD}@crud-alunos.og8ztxw.mongodb.net/?retryWrites=true&w=majority&appName=CRUD-Alunos`
+    )
+    .then(() => {
+        app.listen(3333);
+        console.log("Conectado ao MongoDB!");
+    })
+    .catch((err)=>console.log(err));
+
+ 
