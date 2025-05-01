e6e4e6638f00fa67b259fa91871c03139d96ab70
João Vitor <romanijoaovitor@gmail.com>
2025-05-01 17:56:31 -0300

    adiciona a biblioteca Day.js para registrar os campos createdAt e updatedAt de Student

--- models/Student.js
+++ models/Student.js
@@ -1,12 +1,19 @@
 const mongoose = require('mongoose');
+const dayjs = require('dayjs');
 
 const Student = mongoose.model('Student', {
     name: String,
     age: Number,
     ra: String,
     cpf: String,
-    createdAt: String,
-    updatedAt: String
+    createdAt: {
+        type: String,
+        default: () => dayjs().format('DD/MM/YYYY HH:mm:ss')
+    },
+    updatedAt: {
+        type: String,
+        default: () => dayjs().format('DD/MM/YYYY HH:mm:ss')
+    }
 });
 
 module.exports = Student;
