4bb3fa6cd066335742dc44bfccaef94ec33ea6b4
João Vitor <romanijoaovitor@gmail.com>
2025-05-01 18:17:58 -0300

    cria rota POST para cadatrar um aluno

--- routes/studentRoutes.js
+++ routes/studentRoutes.js
@@ -3,4 +3,39 @@ const router = require('express').Router();
 // Importando Model
 const Student = require('../models/Student');
 
+// Cadastro de aluno
+router.post('/', async (req, res) => {
+    const { name, age, ra, cpf } = req.body;
+
+    if(!name) {
+        res.status(422).json({error: "O nome do aluno é obrigatório!"});
+        return;
+    }
+
+    if(!ra) {
+        res.status(422).json({error: "O ra do aluno é obrigatório!"});
+        return;
+    }
+
+    if(!cpf) {
+        res.status(422).json({error: "O cpf do aluno é obrigatório!"});
+        return;
+    }
+
+    const student = {
+        name,
+        age,
+        ra,
+        cpf
+    };
+
+    try {
+        await Student.create(student);
+        res.status(201).json({ message: "Aluno cadastrado com sucesso!" });
+    } 
+    catch (err) {
+        res.status(500).json({ error: err });
+    }
+})
+
 module.exports = router;
