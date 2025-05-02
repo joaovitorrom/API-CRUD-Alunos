faf82413f522d3858e39807693705511cd906dce
João Vitor <romanijoaovitor@gmail.com>
2025-05-02 16:21:42 -0300

    cria Schema e adiciona middleware para atualizar o atributo updatedAt

--- models/Student.js
+++ models/Student.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const dayjs = require('dayjs');
 
-const Student = mongoose.model('Student', {
+const studentSchema = new mongoose.Schema({
     name: { type: String, required: true },
     age: { type: Number, required: true },
     ra: { type: String, required: true, unique: true },
@@ -15,5 +15,13 @@ const Student = mongoose.model('Student', {
         default: () => dayjs().format('DD/MM/YYYY HH:mm:ss')
     }
 });
+
+// Middleware do Moongose para atualizar o atributo updatedAt
+studentSchema.pre('updateOne', function (next) {
+    this._update.updatedAt = dayjs().format('DD/MM/YYYY HH:mm:ss');
+    next();
+});
+
+const Student = mongoose.model('Student', studentSchema);
 
 module.exports = Student;
